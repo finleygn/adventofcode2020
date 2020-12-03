d638745e8cc88d7e19a58b9886b11717249b4482
finleygn <finleygarton12@gmail.com>
2020-12-03 08:31:29 +0000

    initial day 3 solution

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,18 @@ mod util {
         path::Path,
     };
 
+    pub fn grid_from_file(filename: impl AsRef<Path>) -> Vec<Vec<char>> {
+        let file = File::open(filename).expect("no such file");
+        let buf = BufReader::new(file);
+        buf.lines()
+            .map(|l| {
+                l.expect("Could not parse line")
+                    .chars()
+                    .collect::<Vec<char>>()
+            })
+            .collect::<Vec<Vec<char>>>()
+    }
+
     pub fn lines_from_file_as_u32(filename: impl AsRef<Path>) -> Vec<u32> {
         let file = File::open(filename).expect("no such file");
         let buf = BufReader::new(file);
@@ -64,7 +76,6 @@ mod day1 {
     #[cfg(test)]
     mod tests {
         use super::{super::util, *};
-        
         #[test]
         fn d1part1test() {
             let data = util::lines_from_file_as_u32("./data/day1.txt");
@@ -159,6 +170,66 @@ mod day2 {
         fn d2part2test() {
             let data = util::lines_from_file("./data/day2.txt");
             assert_eq!(part2(data), 611);
+        }
+    }
+}
+
+#[allow(dead_code)]
+mod day3 {
+    pub struct Pos {
+        x: usize,
+        y: usize
+    }
+
+    pub fn traverse(map: &Vec<Vec<char>>, inc: Pos) -> u32 {
+        let mut total = 0;
+        let mut pos = Pos {
+            x: 0,
+            y: 0,
+        };
+        
+        while pos.y < map.len() {
+            if map[pos.y][pos.x % map[pos.y].len()] == '#' {
+                total += 1;
+            }
+
+            pos.x += inc.x;
+            pos.y += inc.y;
+        }
+
+        total
+    }
+
+    pub fn part1(map: Vec<Vec<char>>) -> u32 {
+        traverse(&map, Pos{ x: 3, y: 1 })
+    }
+
+    pub fn part2(map: Vec<Vec<char>>) -> u32 {
+        let rounds = vec![
+            traverse(&map, Pos{ x: 1, y: 1 }),
+            traverse(&map, Pos{ x: 3, y: 1 }),
+            traverse(&map, Pos{ x: 5, y: 1 }),
+            traverse(&map, Pos{ x: 7, y: 1 }),
+            traverse(&map, Pos{ x: 1, y: 2 }),
+        ];
+
+        rounds.into_iter().product()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::{super::util, *};
+
+        #[test]
+        fn d3part1test() {
+            let data = util::grid_from_file("./data/day3.txt");
+            assert_eq!(part1(data), 247);
+        }
+
+        #[test]
+        fn d3part2test() {
+            let data = util::grid_from_file("./data/day3.txt");
+            assert_eq!(part2(data), 2983070376);
         }
     }
 }
